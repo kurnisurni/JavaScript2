f32d9e91a55e6173a53631b7f7143ce62a8dac1f
Kurnia <kurniasuryani@gmail.com>
2021-03-12 01:15:41 +0100

    test successfully

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
+    // Replace this comment and the next line with your code
+    //filtered even numbers
+    const oddNumber = numbers.filter((odd) => odd % 2 !== 0);
+    //multiply the value to 2 using map
+    numbers = oddNumber.map((double) => double * 2);
+    return numbers;
 }
 
 const myNumbers = [1, 2, 3, 4];
@@ -10,6 +14,6 @@ console.log(doubleOddNumbers(myNumbers));
 
 // Do not change or remove anything below this line
 module.exports = {
-  myNumbers,
-  doubleOddNumbers,
+    myNumbers,
+    doubleOddNumbers,
 };
