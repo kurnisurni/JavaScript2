0b22f076f664e67ffa69a94b52a2b06073f27494
Kurnia <kurniasuryani@gmail.com>
2021-03-21 00:13:37 +0100

    fixing 2-2

--- Week3/homework/step2-2.js
+++ Week3/homework/step2-2.js
@@ -10,26 +10,25 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
     for (let i = startIndex; i <= stopIndex; i++) { numbers.push(i); }
 
     numbers.forEach(number => {
-        // eslint-disable-next-line no-func-assign
-        sayThree = threeCallback(number);
-        // eslint-disable-next-line no-func-assign
-        sayFive = fiveCallback(number);
-
+        if (number % 3 === 0 && number % 5 === 0) {
+            threeCallback(number);
+            fiveCallback(number);
+        } else if (number % 3 === 0) {
+            threeCallback(number);
+        } else if (number % 5 === 0) {
+            fiveCallback(number);
+        }
     });
 
 }
 
 function sayThree(number) {
     // eslint-disable-next-line eqeqeq
-    if (number % 3 == 0) {
-        console.log('Say three!');
-    }
+    console.log(number + ' is multiplication of three.');
 }
 
 function sayFive(number) {
-    if (number % 5 == 0) {
-        console.log('Say five!');
-    }
+    console.log(number + ' is multiplication of five.');
 }
 
 threeFive(10, 15, sayThree, sayFive);
