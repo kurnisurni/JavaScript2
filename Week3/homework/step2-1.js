fa3a2261574a948f551a6d1f0622d500709e449f
Kurnia <kurniasuryani@gmail.com>
2021-03-21 00:13:26 +0100

    fixing 2-1

--- Week3/homework/step2-1.js
+++ Week3/homework/step2-1.js
@@ -1,16 +1,18 @@
+/* eslint-disable prettier/prettier */
+
 'use strict';
 
 function foo(func) {
     // What to do here?
     // Replace this comment and the next line with your code
-    return func;
+    func();
 }
 
 function bar() {
     console.log('Hello, I am bar!');
 }
 
-foo(bar());
+foo(bar);
 
 // Do not change or remove anything below this line
 module.exports = foo;
